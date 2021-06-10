0e89eadacc7dbb59934733a8d330f7f0e2d90f11
Ben Tovee <ben.tovee@gmail.com>
2021-06-10 17:42:13 +0100

    final set up with node-cron

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 const axios = require('axios');
+const cron = require('node-cron');
+const fs = require('fs');
+const util = require('util');
+const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+const log_stdout = process.stdout;
+
+console.log = (d) => {
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
 
 const kibisisLiquidityPoolAddress = '0x475eC28dDb314e349248ff4f4612FdE79717089B';
 const faucet1 = 'https://faucet.ropsten.be/donate/';
@@ -12,11 +22,8 @@ const getEth = async () => {
     }
 }
 
-(async () => {
-    try {
-        const response = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
-        console.log(response.data.explanation);
-    } catch (error) {
-        console.error(error);
-    }
-})()
+
+
+cron.schedule('0 14 * * *', async () => {
+   await getEth();
+});
